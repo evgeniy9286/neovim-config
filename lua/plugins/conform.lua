47b26d665f2dcd5c5f69d44c1905848fb22d29a3
Evgeniy Makhnin <evgeniymakhnin@gmail.com>
2025-03-23 10:26:14 +0200

    Initial commit

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,27 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					go = { "gopls" },
+					templ = {
+						"templ",
+					},
+				},
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({ bufnr = args.buf })
+				end,
+			})
+		end,
+	},
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,55 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("nvim-autopairs").setup({
+				disable_filetype = { "TelescopePrompt", "vim" },
+			})
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({ settings = { Lua = { diagnostics = { globals = { "vim", "require" } } } } })
+			lspconfig.gopls.setup({
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+			})
+			lspconfig.cmake.setup({})
+			lspconfig.ts_ls.setup({})
+			lspconfig.templ.setup({
+				filetypes = { "templ" },
+			})
+			lspconfig.vuels.setup({})
+			lspconfig.eslint.setup({})
+			lspconfig.html.setup({
+				filetypes = { "html", "templ" },
+			})
+			lspconfig.htmx.setup({})
+			lspconfig.tailwindcss.setup({
+				filetypes = { "templ", "astro", "javascript", "typescript", "react", "html", "css" },
+				settings = {
+					tailwindCSS = {
+						includeLanguages = {
+							templ = "html",
+						},
+					},
+				},
+			})
+			vim.filetype.add({ extension = { templ = "templ" } })
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename Symbol" })
+					vim.keymap.set({ "n", "v" }, "<Leader>la", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<Leader>lf", function()
+						vim.lsp.buf.format({ async = true })
+					end, opts)
+				end,
+			})
+		end,
+	},
+}
